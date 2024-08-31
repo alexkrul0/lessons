1c052039ff962ca9a4bae8a1e328b5a500737bc0
alexkrul0 <alexkrul0@gmail.com>
2024-08-31 15:32:39 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+num_of_houmwork = 12
+sum_of_hourses = 1.5
+name_of_cours = 'Python'
+time_task = sum_of_hourses / num_of_houmwork
+print('Курс:', name_of_cours, ', всего задач:', num_of_houmwork, ', затрачено часов:', sum_of_hourses, ', среднее время выполнения', time_task, 'часа.' )
