14ed5309439e07522ac895a7f5afce35ea500e09
alexkrul0 <alexkrul0@gmail.com>
2024-09-03 19:41:05 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (1, 5, 'string', True, 2.5)
+print(immutable_var)
+#immutable_var[1] = 6
+print(immutable_var)
+
+mutable_list = [6, 7, 'name', False, 1.8]
+mutable_list[1] = 5
+mutable_list[3] = True
+print(mutable_list)
