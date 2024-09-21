ab408cef78e6010b003db6ff10cb43238692d8e1
alexkrul0 <alexkrul0@gmail.com>
2024-09-21 20:34:50 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,11 +1,14 @@
 def get_multiplied_digits(number):
+    if number % 10 == 0:
+        number += 1
+
     str_number = str(number)
     first = int(str_number[0])
+
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
-    #print(first)
 
-print(get_multiplied_digits(40203))
+print(get_multiplied_digits(240))
