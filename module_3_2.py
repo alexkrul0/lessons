2cb4e9aab84f5675675d54ed7a18c1f5a4730a08
alexkrul0 <alexkrul0@gmail.com>
2024-09-24 16:41:28 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,10 +1,24 @@
 
 def send_email(message, recipient, *, sender='university.help@gmail.com'):
 
-    if (('@' not in sender) or ('@' not in recipient)) or ((not sender.endswith('.com') or not recipient.endswith('.com'))
-        and (not sender.endswith('.ru') or not recipient.endswith('.ru')) and
-        (not sender.endswith('.net') or not recipient.endswith('.net'))):
+    # if ('@' and ('.com' or '.ru' or '.net')) not in (recipient or sender):
+    #     print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+
+    if '@' not in recipient or '@' not in sender or not (recipient.endswith(('.com', '.ru', '.net'))
+            and sender.endswith(('.com', '.ru', '.net'))):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+
+
+
+    # if ((('@' not in sender) or ('@' not in recipient)) or
+    #         (not (sender.endswith('.com') or not (recipient.endswith('.com')) or
+    #          not (sender.endswith('.ru') or not (recipient.endswith('.ru')) or
+    #          not (sender.endswith('.net') or not (recipient.endswith('.net'))))))):
+    #     print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+    # elif ((not sender.endswith('.com') and not recipient.endswith('.com')) and
+    #     (not sender.endswith('.ru') and not recipient.endswith('.ru')) and
+    #     (not sender.endswith('.net') and not recipient.endswith('.net'))):
+    #     print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
 
     elif sender == recipient:
         print('Нельзя отправить письмо самому самому себе!')
