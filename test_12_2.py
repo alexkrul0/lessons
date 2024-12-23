2cffac0cef45e64ee3d2bea3841164900d177bca
alexkrul0 <alexkrul0@gmail.com>
2024-12-23 16:54:26 +0300

    Add files via upload

--- test_12_2.py
+++ test_12_2.py
@@ -5,6 +5,8 @@ from runner_and_tournament import Runner, Tournament
 
 
 class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
     def setUp(self):
         self.run1 = Runner('Усэйн', 10)
         self.run2 = Runner('Андрей', 9)
@@ -14,28 +16,27 @@ class TournamentTest(unittest.TestCase):
     def setUpClass(cls):
         cls.all_results = {}
 
-
     def tearDown(self):
         sort_result = {}
         for time, runner in sorted(self.all_results.items()):
             sort_result[time] = runner.name
         print(sort_result)
 
-
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test1(self):
         tournament = Tournament(90, self.run1, self.run3)
         result = tournament.start()
         self.all_results.update(result)
         self.assertTrue(self.all_results[max(self.all_results)] == 'Ник')
 
-
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test2(self):
         tournament = Tournament(90, self.run2, self.run3)
         result = tournament.start()
         self.all_results.update(result)
         self.assertTrue(self.all_results[max(self.all_results)] == 'Ник')
 
-
+    @unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test3(self):
         tournament = Tournament(90, self.run1, self.run2, self.run3)
         result = tournament.start()
